1e809d38f3c741ae2d7b5ca923ed52013cbb0bf6
Ícaro Moreira <127455211+IcaroMoreir4@users.noreply.github.com>
2025-05-21 14:37:28 -0300

    feat: iniciando naveçaão entre pages (Login)

--- FleetMaxAndroidApp/app/src/main/java/com/example/fleetmax_androidapp/Login.kt
+++ FleetMaxAndroidApp/app/src/main/java/com/example/fleetmax_androidapp/Login.kt
@@ -1,20 +1,20 @@
-package com.example.fleetmax_androidapp
+import android.content.Intent
+import android.widget.TextView
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(R.layout.activity_login)
 
-class Login : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        insets
+    }
+
+    val comeceAqui = findViewById<TextView>(R.id.textView8)
+    comeceAqui.setOnClickListener {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
